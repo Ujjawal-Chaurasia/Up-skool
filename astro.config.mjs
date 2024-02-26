4e18430d9975fa6be741fe0fe549fa22b5cf6efd
Ujjawal-Chaurasia <chaurasiaujjawal208@gmail.com>
2024-02-26 10:26:44 +0530

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+// import node from "@astrojs/node";
 
-import node from "@astrojs/node";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https:/Ujjawal-Chaurasia/.github.io',
-  base: '/Up-skool',
-  output: "static",
+  site: "https:/Ujjawal-Chaurasia/.github.io",
+  base: "/Up-skool",
+  output: "server",
   integrations: [tailwind(), react()],
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify({
+    edgeMiddleware: true,
+  }),
 });
